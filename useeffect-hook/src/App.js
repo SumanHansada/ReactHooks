54481227015c6c168912dc0faa598e73d575d4cb
Suman Hansada <suman.hansada@gmail.com>
2021-09-11 09:22:54 +0530

    Initial Setup useeffect-hook

---
+++ useeffect-hook/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ClassCounterOne from "./components/ClassCounterOne";
+
+function App() {
+  return (
+    <div className="App">
+      <ClassCounterOne />
+    </div>
+  );
+}
+
+export default App;
