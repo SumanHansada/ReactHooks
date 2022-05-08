a80bf547c923bba05b858dee9f8ef5dbed608303
Suman Hansada <suman.hansada@gmail.com>
2022-05-08 15:18:46 +0530

    Added missing code in useReducer hook

--- usereducer-hook/src/App.js
+++ usereducer-hook/src/App.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./App.css";
+import ComponentA from "./components/ComponentA";
+import ComponentB from "./components/ComponentB";
+import ComponentC from "./components/ComponentC";
+import CounterOne from "./components/CounterOne";
+import CounterThree from "./components/CounterThree";
+import CounterTwo from "./components/CounterTwo";
 import DataFetchingOne from "./components/DataFetchingOne";
 import DataFetchingTwo from "./components/DataFetchingTwo";
 
+export const CounterContext = React.createContext();
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    case "RESET":
+      return initialState;
+    default:
+      return state;
+  }
+};
+
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState);
   return (
     <div className="App">
+      {/* <CounterContext.Provider
+        value={{ countState: count, countDispatch: dispatch }}
+      >
+        <ComponentA />
+        <ComponentB />
+        <ComponentC />
+      </CounterContext.Provider> */}
+      {/* <CounterOne /> */}
+      {/* <CounterTwo /> */}
+      {/* <CounterThree /> */}
       {/* <DataFetchingOne /> */}
       <DataFetchingTwo />
     </div>
